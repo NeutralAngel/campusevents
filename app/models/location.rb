3e4738b9f49cc88c7e9f0a0c1346dc5910643eb3
Nathan Nontell <nathan.nontell@gmail.com>
2014-02-18 20:43:53 -0500

    buncha stuff

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ActiveRecord::Base
   validates :name, presence: true
   has_many :meetings
+
+  def upcoming_meetings
+    meetings.where(['start_time > ?', DateTime.now]).sort{|a,b| a.start_time <=> b.start_time}.first(5)
+  end
 end
