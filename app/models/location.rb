5a8025f70825adac0660ca188c7b8d41bc6b07ef
Nathan Nontell <nathan.nontell@gmail.com>
2014-02-18 21:41:33 -0500

    finishing touches, yo

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,10 @@ class Location < ActiveRecord::Base
   has_many :meetings
 
   def upcoming_meetings
-    meetings.where(['start_time > ?', DateTime.now]).sort{|a,b| a.start_time <=> b.start_time}.first(5)
+    future_meetings.sort{|a,b| a.start_time <=> b.start_time}.first(5)
+  end
+
+  def future_meetings
+    meetings.where(['start_time > ?', DateTime.now])
   end
 end
